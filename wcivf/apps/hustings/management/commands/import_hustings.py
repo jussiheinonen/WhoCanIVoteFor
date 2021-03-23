b53a10e587eb76b9a38da7d55d8a6298b5410581
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-03-23 17:39:58 +0000

    Update hustings import management command for 2021
    - Import hustings via urls by default
    - Tried to keep backwards compatible so old hustings could be imported
    - Optionally import from file

--- wcivf/apps/hustings/management/commands/import_hustings.py
+++ wcivf/apps/hustings/management/commands/import_hustings.py
@@ -2,7 +2,6 @@
 Importer for all our important Hustings data
 """
 import os
-import csv
 import datetime
 
 from django.core.management.base import BaseCommand
@@ -11,21 +10,27 @@ from django.utils import timezone
 
 from elections.models import PostElection
 from hustings.models import Husting
+from hustings.importers import HustingImporter
 
 
 def dt_from_string(dt):
     """
     Given a date string DT, return a datetime object.
-    Try multiple strptime formats b/c Google sheets doesn't
-    understand counting to three.
     """
-    date = None
+    try:
+        date = datetime.datetime.strptime(dt, "%Y-%m-%d")
+    except ValueError:
+        pass
+    else:
+        return timezone.make_aware(date, timezone.get_current_timezone())
+
+    # kept for legacy reasons - previous years used different date formatting
     try:
         date = datetime.datetime.strptime(dt, "%Y-%b-%d")
     except ValueError:
         date = datetime.datetime.strptime(dt, "%Y-%B-%d")
-    if date:
-        return timezone.make_aware(date, timezone.get_current_timezone())
+
+    return timezone.make_aware(date, timezone.get_current_timezone())
 
 
 def stringy_time_to_inty_time(stringy_time):
@@ -49,9 +54,22 @@ def set_time_string_on_datetime(dt, time_string):
 
 
 class Command(BaseCommand):
+
+    GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQIvJtlR9j6BaOuLjd501W4mPznJX41k1LalgkXvwRLj0d90lyFaW3PoVLzLvw5K4Kvv4lETQIJyzdL/"
+    URLS = [
+        f"{GOOGLE_SHEET_URL}pub?gid=0&single=true&output=csv",
+        f"{GOOGLE_SHEET_URL}pub?gid=742711428&single=true&output=csv",
+        f"{GOOGLE_SHEET_URL}pub?gid=908428564&single=true&output=csv",
+        f"{GOOGLE_SHEET_URL}pub?gid=1265695055&single=true&output=csv",
+        f"{GOOGLE_SHEET_URL}pub?gid=668485812&single=true&output=csv",
+        f"{GOOGLE_SHEET_URL}pub?gid=1596983142&single=true&output=csv",
+    ]
+
     def add_arguments(self, parser):
         parser.add_argument(
-            "filename", help="Path to the file with the hustings in it"
+            "--filename",
+            required=False,
+            help="Path to the file with the hustings in it",
         )
         parser.add_argument(
             "--quiet",
@@ -60,27 +78,14 @@ class Command(BaseCommand):
             default=False,
             help="Only output errors",
         )
-        parser.add_argument(
-            "--for-date",
-            action="store",
-            dest="date",
-            required=True,
-            help="The date of the elections this file is about",
-        )
-
-    def delete_all_hustings(self, election_date):
-        """
-        Clear our hustings away.
-        """
-        Husting.objects.filter(
-            post_election__election__election_date=election_date
-        ).delete()
 
     def create_husting(self, row):
         """
         Create an individual husting
         """
-        starts = dt_from_string(row["Date (YYYY-Month-DD)"])
+        # kept the second option to work with previous years spreadsheets
+        starts = row.get("Date (YYYY-MM-DD)") or row.get("Date (YYYY-Month-DD)")
+        starts = dt_from_string(starts)
         ends = None
         if row["Start time (00:00)"]:
             starts = set_time_string_on_datetime(
@@ -97,19 +102,38 @@ class Command(BaseCommand):
             # This might be a parent election ID
             pes = PostElection.objects.filter(election__slug=row["Election ID"])
         for pe in pes:
-            husting = Husting(
+            husting = Husting.objects.create(
                 post_election=pe,
                 title=row["Title of event"],
                 url=row["Link to event information"],
                 starts=starts,
                 ends=ends,
-                location=row["Name of event location (e.g. Church hall)"],
-                postcode=row["Postcode of event location"],
+                location=row.get("Name of event location (e.g. Church hall)"),
+                postcode=row.get("Postcode of event location"),
                 postevent_url=row[
                     "Link to post-event information (e.g. blog post, video)"
                 ],
             )
-            husting.save()
+            return husting
+
+    def import_hustings(self):
+        for row in self.importer.rows:
+            try:
+                husting = self.create_husting(row)
+            except ValueError as e:
+                self.stdout.write(repr(e))
+                husting = None
+
+            if not husting:
+                self.stdout.write(f"Couldn't create {row['Title of event']}")
+                continue
+
+            self.hustings_counter += 1
+            self.stdout.write(
+                "Created husting {0} <{1}>".format(
+                    self.hustings_counter, husting
+                )
+            )
 
     @transaction.atomic
     def handle(self, **options):
@@ -119,22 +143,14 @@ class Command(BaseCommand):
         if options["quiet"]:
             self.stdout = open(os.devnull, "w")
 
-        for_election = options["date"]
+        count, _ = Husting.objects.all().delete()
+        self.stdout.write(f"Deleted {count} Husting objects")
 
-        self.delete_all_hustings(for_election)
-        hustings_counter = 0
-        self.not_a_constituency_friend = []
-        with open(options["filename"], "r") as fh:
-            reader = csv.DictReader(fh)
-            for row in reader:
-                try:
-                    husting = self.create_husting(row)
-                except:
-                    husting = None
-                if husting:
-                    hustings_counter += 1
-                    self.stdout.write(
-                        "Created husting {0} <{1}>".format(
-                            hustings_counter, husting
-                        )
-                    )
+        self.hustings_counter = 0
+        if options["filename"]:
+            self.importer = HustingImporter(file_path=options["filename"])
+            return self.import_hustings()
+
+        for url in self.URLS:
+            self.importer = HustingImporter(url=url)
+            self.import_hustings()
