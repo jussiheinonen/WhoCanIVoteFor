7d92e091f9bae5d379dba46c7d87b0f1700653cc
Virginia Dooley <vcdooley@gmail.com>
2021-07-14 15:26:15 +0100

    Test valid html and fix errors

--- wcivf/apps/core/tests/test_frontend.py
+++ wcivf/apps/core/tests/test_frontend.py
@@ -5,12 +5,19 @@ Used for making sure meta tags and important information is actually
 shown before and after template changes.
 """
 
-
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.urls import reverse
+from dc_utils.tests.helpers import validate_html
+from people.tests.factories import PersonFactory, PersonPostFactory
+from parties.tests.factories import PartyFactory
+from elections.tests.factories import (
+    ElectionFactory,
+    PostElectionFactory,
+)
 
 import vcr
+import pytest
 
 
 @override_settings(
@@ -28,3 +35,44 @@ class TestMetaTags(TestCase):
         for name, url in self.important_urls.items():
             req = self.client.get(url)
             assert req.status_code == 200
+
+
+class TestHtml:
+    @pytest.fixture
+    def urls(self):
+        return [
+            reverse("home_view"),
+            reverse("about_view"),
+            reverse("standing_as_a_candidate"),
+            reverse("opensearch"),
+            reverse("status_check_view"),
+            PersonPostFactory(
+                election=ElectionFactory()
+            ).person.get_absolute_url(),
+            reverse(
+                "email_person_view",
+                kwargs={"pk": PersonFactory().pk, "ignored_slug": "sadiq-khan"},
+            ),
+            reverse("parties_view"),
+            PartyFactory().get_absolute_url(),
+            reverse("elections_view"),
+            ElectionFactory().get_absolute_url(),
+            PostElectionFactory().get_absolute_url(),
+            reverse("postcode_view", kwargs={"postcode": "EC1A4EU"}),
+            reverse("results_list_view"),
+            reverse("api:api-root"),
+            reverse("dc_signup_form:mailing_list_signup_view"),
+            reverse("feedback_form_view"),
+            reverse("sv_voting_system_view"),
+            reverse("fptp_voting_system_view"),
+            reverse("ams_voting_system_view"),
+        ]
+
+    @pytest.mark.django_db
+    def test_html_valid(self, client, subtests, urls):
+        for url in urls:
+            with subtests.test(msg=url):
+                assert client.get(url).status_code == 200
+                _, errors = validate_html(client, url)
+                print(url, errors)
+                assert errors == ""
