e2cb0e571ad372daa111650da34a6086f00330c7
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-08-24 15:39:07 +0100

    Add debug flags to bash eployscripts

--- deployscripts/clean_instance.sh
+++ deployscripts/clean_instance.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -xeE
 
 # delete all code including hidden files
 rm -rf /var/www/wcivf/code/* /var/www/wcivf/code/.* 2> /dev/null || true
--- deployscripts/install_python_deps.sh
+++ deployscripts/install_python_deps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -xeE
 
 # should we delete the env and recreate?
 source /var/www/wcivf/env/bin/activate
