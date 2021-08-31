c37beeab78a67c7da3474a673052dd327d47bbdf
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-08-31 18:01:05 +0100

    Update how parameters are retrieved - Set max num items to be returned from param store - Update envfile script to use get_parameter

--- deployscripts/write_envfile.py
+++ deployscripts/write_envfile.py
@@ -5,17 +5,17 @@ client = boto3.client("ssm", region_name="eu-west-2")
 
 
 def get_parameter_names():
-    response = client.describe_parameters()
+    response = client.describe_parameters(MaxResults=50)
     return [param["Name"] for param in response["Parameters"]]
 
 
 def write_parameters_to_envfile():
     names = get_parameter_names()
-    response = client.get_parameters(Names=names)
     with open("/var/www/wcivf/code/.env", "w") as f:
-        for parameter in response["Parameters"]:
-            key = parameter["Name"]
-            value = parameter["Value"]
+        for name in names:
+            response = client.get_parameter(Name=name)
+            key = response["Parameter"]["Name"]
+            value = response["Parameter"]["Value"]
             f.write(f"{key}={value}\n")
 
 
