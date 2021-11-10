ee83d12c6ff2570bcc429837bc33c23117661f5a
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-11-10 15:59:15 +0000

    Update CodeDeploy BeforeInstall step
    Remove the StopApplication step as I dont think is used in a blue
    green deployments. Instead strop services in the BeforeInstall step

--- deployscripts/stop_application.sh
+++ deployscripts/stop_application.sh
@@ -3,3 +3,6 @@ set -xeE
 
 service wcivf_gunicorn stop
 service wcivf_db_replication stop
+
+# delete all code including hidden files
+rm -rf /var/www/wcivf/code/* /var/www/wcivf/code/.* 2> /dev/null || true
