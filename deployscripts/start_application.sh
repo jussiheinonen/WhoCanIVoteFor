5e7494837900a5658b92eba0b4f9fa523b9e4ee8
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-11-11 15:47:11 +0000

    Enable gunicorn/replication with codedeploy
    These steps have been removed from image provisioning, so instead
    enable them with codedeploy. This will mean the services dont start on
    initial boot before the latest code has been pulled, but the services
    will still restart if the instance reboots.

--- deployscripts/start_application.sh
+++ deployscripts/start_application.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -xeE
 
-service wcivf_db_replication start
-service wcivf_gunicorn start
+# enabling will allow the services to start if the instance reboots
+systemctl enable wcivf_db_replication.service
+systemctl enable wcivf_gunicorn.socket
+
+systemctl start wcivf_db_replication.service
+systemctl start wcivf_gunicorn.socket
