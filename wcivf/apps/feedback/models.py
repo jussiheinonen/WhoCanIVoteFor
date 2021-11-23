2c7b9c0ea6fd6df4fa53f9b2f6b0ad66e53d5794
Virginia Dooley <vcdooley@gmail.com>
2021-11-23 16:00:43 +0000

    Add voting question to feedback form

--- wcivf/apps/feedback/models.py
+++ wcivf/apps/feedback/models.py
@@ -3,12 +3,14 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 FOUND_USEFUL_CHOICES = (("YES", "Yes"), ("NO", "No"))
+VOTE_CHOICES = (("YES", "Yes"), ("NO", "No"))
 
 
 class Feedback(TimeStampedModel):
     found_useful = models.CharField(
         blank=True, max_length=100, choices=FOUND_USEFUL_CHOICES
     )
+    vote = models.CharField(blank=True, max_length=100, choices=VOTE_CHOICES)
     sources = models.TextField(blank=True)
     comments = models.TextField(blank=True)
     source_url = models.CharField(blank=True, max_length=800)
