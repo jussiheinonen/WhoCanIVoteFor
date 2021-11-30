410012fc20d003a509355d3a622aec1f65d8cd9e
Virginia Dooley <vcdooley@gmail.com>
2021-11-30 11:18:58 +0000

    Add request to canonical_url

--- wcivf/apps/core/context_processors.py
+++ wcivf/apps/core/context_processors.py
@@ -5,7 +5,7 @@ from .forms import PostcodeLookupForm
 
 
 def canonical_url(request):
-    return {"CANONICAL_URL": f"{request.scheme}://{request.get_host}"}
+    return {"CANONICAL_URL": f"{request.scheme}://{request.get_host()}"}
 
 
 def site_title(request):
