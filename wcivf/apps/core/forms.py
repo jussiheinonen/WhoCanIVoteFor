364f34182624393b3dd0b191784e483e5026e8bf
Sym Roe <sym.roe@talusdesign.co.uk>
2022-03-04 12:34:47 +0000

    Initial home page translations

--- wcivf/apps/core/forms.py
+++ wcivf/apps/core/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from localflavor.gb.forms import GBPostcodeField
 
 
 class PostcodeLookupForm(forms.Form):
-    postcode = GBPostcodeField(label="Enter your postcode")
+    postcode = GBPostcodeField(label=_("Enter your postcode"))
 
     def __init__(self, autofocus=False, *args, **kwargs):
         super(PostcodeLookupForm, self).__init__(*args, **kwargs)
