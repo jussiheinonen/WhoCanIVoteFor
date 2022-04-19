02898f0a071c08d02cb38c5390a47fd6b55e8914
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-04-19 13:03:24 +0100

    Update wikipedia importer
    To allow this to run within lambda time constraints, adds a current
    flag to limit person queryset to people with current candacies and
    current ballots.

--- wcivf/apps/people/management/commands/import_wikipedia_bios.py
+++ wcivf/apps/people/management/commands/import_wikipedia_bios.py
@@ -2,20 +2,41 @@ from django.core.management.base import BaseCommand
 
 from elections.models import PostElection
 from elections.wikipedia_map import ballot_to_wikipedia
-from people.models import Person
+from people.models import Person, PersonPost
 from people.helpers import get_wikipedia_extract
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--current",
+            action="store_true",
+            dest="current",
+            default=False,
+            help="Only import bios for current candidates",
+        )
+
     def handle(self, **options):
-        for person in Person.objects.exclude(wikipedia_url=None).exclude(
-            wikipedia_url=""
-        ):
+
+        people = Person.objects.exclude(wikipedia_url=None)
+        if options["current"]:
+            current_candidacies = PersonPost.objects.current()
+            people = (
+                people.filter(personpost__in=current_candidacies)
+                .order_by()
+                .distinct()
+            )
+        for person in people:
             person.wikipedia_bio = get_wikipedia_extract(person.wikipedia_url)
             person.save()
 
-        qs = PostElection.objects.filter(ballot_paper_id__startswith="parl.")
-        for ballot in qs:
+        parl_ballots = PostElection.objects.filter(
+            ballot_paper_id__startswith="parl."
+        )
+        if options["current"]:
+            parl_ballots.filter(election__current=True)
+
+        for ballot in parl_ballots:
             start = ".".join(ballot.ballot_paper_id.split(".")[:-1]) + "."
             if start in ballot_to_wikipedia:
                 ballot.wikipedia_url = ballot_to_wikipedia[start]
