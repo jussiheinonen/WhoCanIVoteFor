72eb3d6243eb3c4ea9256f0f17e3d34d45659326
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-04-27 12:57:24 +0100

    Use WDIV api over http to speed up responses

--- wcivf/settings/base.py
+++ wcivf/settings/base.py
@@ -205,7 +205,7 @@ YNR_BASE = "https://candidates.democracyclub.org.uk"
 YNR_UTM_QUERY_STRING = "utm_source=who&utm_campaign=ynr_cta"
 EE_BASE = os.environ.get("EE_BASE", "https://elections.democracyclub.org.uk")
 
-WDIV_BASE = "https://wheredoivote.co.uk"
+WDIV_BASE = "http://wheredoivote.co.uk"
 WDIV_API = "/api/beta"
 
 CANONICAL_URL = "https://whocanivotefor.co.uk"
